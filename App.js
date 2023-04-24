13a7345d413c47d036300ce1b52c5739f3bbb242
ciriatico <gabrielciriatico@gmail.com>
2023-04-24 17:06:48 -0300

    Telas de carregamento e inicial

--- App.js
+++ App.js
@@ -3,20 +3,59 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 
 import Login from "./src/screens/Login";
-import MyStatusBar from "./src/components/MyStatusBar";
+import FrontPage from "./src/screens/FrontPage";
+import LoadingPage from "./src/screens/LoadingPage";
+import { useFonts, Courgette_400Regular } from "@expo-google-fonts/courgette";
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    Courgette_400Regular,
+  });
   return (
     <NavigationContainer>
-      <MyStatusBar backgroundColor="#88c9bf" barStyle="light-content" />
       <Drawer.Navigator
-        initialRouteName="Login"
+        initialRouteName="LoadingPage"
         screenOptions={{
           drawerPosition: "left",
         }}
       >
+        <Drawer.Screen
+          name="LoadingPage"
+          component={LoadingPage}
+          options={{
+            title: "Loading",
+            headerShown: false,
+            headerTitle: "",
+            headerShadowVisible: false,
+            headerTitleAlign: "left",
+            headerStyle: {
+              backgroundColor: "#FAFAFA",
+            },
+            headerTintColor: "#88c9bf",
+            headerTitleStyle: {
+              fontSize: 20,
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="FrontPage"
+          component={FrontPage}
+          options={{
+            title: "Início",
+            headerTitle: "",
+            headerShadowVisible: false,
+            headerTitleAlign: "left",
+            headerStyle: {
+              backgroundColor: "#FAFAFA",
+            },
+            headerTintColor: "#88c9bf",
+            headerTitleStyle: {
+              fontSize: 20,
+            },
+          }}
+        />
         <Drawer.Screen
           name="Login"
           component={Login}
@@ -26,7 +65,6 @@ const App = () => {
             headerTitleAlign: "left",
             headerStyle: {
               backgroundColor: "#cfe9e5",
-              height: 56,
             },
             headerTintColor: "#434343",
             headerTitleStyle: {
