e1c2692901ad696cc583ad46d9cc6f380a0adf2c
lucasazuma <azumalucas@gmail.com>
2023-04-25 18:42:57 -0300

    merge resolution

--- App.js
+++ App.js
@@ -17,90 +17,90 @@ const App = () => {
     Courgette_400Regular,
   });
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="LoadingPage"
-        screenOptions={{
-          drawerPosition: "left",
-        }}
-      >
-        <Drawer.Screen
-          name="LoadingPage"
-          component={LoadingPage}
-          options={{
-            title: "Loading",
-            headerShown: false,
-            headerTitle: "",
-            headerShadowVisible: false,
-            headerTitleAlign: "left",
-            headerStyle: {
-              backgroundColor: "#FAFAFA",
-            },
-            headerTintColor: "#88c9bf",
-            headerTitleStyle: {
-              fontSize: 20,
-            },
-          }}
-        />
-        <Drawer.Screen
-          name="FrontPage"
-          component={FrontPage}
-          options={{
-            title: "Início",
-            headerTitle: "",
-            headerShadowVisible: false,
-            headerTitleAlign: "left",
-            headerStyle: {
-              backgroundColor: "#FAFAFA",
-            },
-            headerTintColor: "#88c9bf",
-            headerTitleStyle: {
-              fontSize: 20,
-            },
-          }}
-        />
-        <Drawer.Screen
-          name="Login"
-          component={Login}
-          options={{
-            title: "Login",
-            headerShadowVisible: true,
-            headerTitleAlign: "left",
-            headerStyle: {
-              backgroundColor: "#cfe9e5",
-            },
-            headerTintColor: "#434343",
-            headerTitleStyle: {
-              fontSize: 20,
-            },
-          }}
-        />
-        <Drawer.Screen
-          name="RegisterUser"
-          component={RegisterUser}
-          options={{
-            title: "Cadastro Pessoal",
-            headerShadowVisible: true,
-            headerTitleAlign: "left",
-            headerStyle: {
-              backgroundColor: "#cfe9e5",
-            },
-            headerTintColor: "#434343",
-            headerTitleStyle: {
-              fontSize: 20,
-            },
+    <PaperProvider>
+      <NavigationContainer>
+        <Drawer.Navigator
+          initialRouteName='LoadingPage'
+          screenOptions={{
+            drawerPosition: "left",
           }}
         >
           <Drawer.Screen
-            name='ANIMAL REGISTRATION'
-            component={AnimalRegistration}
+            name='LoadingPage'
+            component={LoadingPage}
             options={{
-              title: "ANIMAL REGISTRATION",
+              title: "Loading",
+              headerShown: false,
+              headerTitle: "",
+              headerShadowVisible: false,
+              headerTitleAlign: "left",
+              headerStyle: {
+                backgroundColor: "#FAFAFA",
+              },
+              headerTintColor: "#88c9bf",
+              headerTitleStyle: {
+                fontSize: 20,
+              },
+            }}
+          />
+          <Drawer.Screen
+            name='FrontPage'
+            component={FrontPage}
+            options={{
+              title: "Início",
+              headerTitle: "",
+              headerShadowVisible: false,
+              headerTitleAlign: "left",
+              headerStyle: {
+                backgroundColor: "#FAFAFA",
+              },
+              headerTintColor: "#88c9bf",
+              headerTitleStyle: {
+                fontSize: 20,
+              },
+            }}
+          />
+          <Drawer.Screen
+            name='Login'
+            component={Login}
+            options={{
+              title: "Login",
               headerShadowVisible: true,
               headerTitleAlign: "left",
               headerStyle: {
                 backgroundColor: "#cfe9e5",
-                height: 56,
+              },
+              headerTintColor: "#434343",
+              headerTitleStyle: {
+                fontSize: 20,
+              },
+            }}
+          />
+          <Drawer.Screen
+            name='RegisterUser'
+            component={RegisterUser}
+            options={{
+              title: "Cadastro Pessoal",
+              headerShadowVisible: true,
+              headerTitleAlign: "left",
+              headerStyle: {
+                backgroundColor: "#cfe9e5",
+              },
+              headerTintColor: "#434343",
+              headerTitleStyle: {
+                fontSize: 20,
+              },
+            }}
+          />
+          <Drawer.Screen
+            name='AnimalRegistration'
+            component={AnimalRegistration}
+            options={{
+              title: "Cadastro de Animal",
+              headerShadowVisible: true,
+              headerTitleAlign: "left",
+              headerStyle: {
+                backgroundColor: "#cfe9e5",
               },
               headerTintColor: "#434343",
               headerTitleStyle: {
